6d508a5295450c0ca0579834fdfb0267a060b40b
Matt Carlson and Molly McGlone <matt+molly@>
2016-09-21 15:03:39 -0700

    allow user to add new foods to the database

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,24 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      flash[:notice] = "Food added to the database!"
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = "There was a problem adding your food, please try again"
+      render new
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:name, :calories_per_serving, :serving_size)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   root :to => "foods#index"
 
-  resources :foods, :only => [:index]
+  resources :foods, :only => [:index, :new, :create]
 
 end
