5a1c372688e666e10b557cb0a2e890b4632f45ac
Matt Carlson and Molly McGlone <matt+molly@>
2016-09-21 11:27:40 -0700

    add initial routes, users/show and foods/index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
+
+  resources :users, :only => [:show] do
+    resources :consumptions
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => "foods#index"
+
+  resources :foods, :only => [:index]
+
 end
