1b69191c9e7c93845ce109f343a57c166f9f046a
Matt Carlson and Molly McGlone <matt+molly@>
2016-09-22 11:30:30 -0700

    add methods to show only today's consumptions and today's total calories on user/show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
+    @todays_consumptions = Consumption.where("created_at::date =?", Date.today)
   end
 end
